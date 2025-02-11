1e6e7b86c09004efd097d66a154e8d8f314d6168
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-02-12 00:56:08 +0530

    Convert Sorted Array to Binary Search Tree

---
+++ Divide and Conquer/Convert Sorted Array to Binary Search Tree.rs
@@ -0,0 +1,40 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::helper(&nums, 0, nums.len() as i32 - 1)
+    }
+
+    fn helper(nums: &Vec<i32>, left: i32, right: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if left > right {
+            return None;
+        }
+
+        let mid = (left + right) / 2;
+        let mut node = TreeNode::new(nums[mid as usize]);
+        node.left = Self::helper(nums, left, mid-1);
+        node.right = Self::helper(nums, mid + 1, right);
+
+        Some(Rc::new(RefCell::new(node)))
+    }
+}
