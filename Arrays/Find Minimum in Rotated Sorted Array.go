34cbdb70ee99d7b8ad3fab097f0acd41b123f661
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-02-15 01:35:20 +0530

    Find Minimum in Rotated Sorted Array

---
+++ Arrays/Find Minimum in Rotated Sorted Array.go
@@ -0,0 +1,16 @@
+func findMin(nums []int) int {
+    left := 0
+    right := len(nums) - 1
+
+    for left < right {
+        mid := (left + right) / 2
+
+        if nums[mid] > nums[right] {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return nums[right]
+}
