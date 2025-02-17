b67be934a033d51a5934b404b20a30b7c156413d
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-02-18 01:41:36 +0530

    Factorial Trailing Zeroes

---
+++ Arrays/Factorial Trailing Zeroes.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn trailing_zeroes(mut n: i32) -> i32 {
+        let mut res = 0;
+
+        while n > 0 {
+            res += n/5;
+            n /= 5;
+        }
+        res
+    }
+}
