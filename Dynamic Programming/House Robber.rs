986090efb42d059211f4f8d731c4837d5a776c20
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-02-20 02:13:28 +0530

    House Robber

--- Dynamic Programming/House Robber.rs
+++ Dynamic Programming/House Robber.rs
@@ -1,4 +1,20 @@
-// Memoization Bottom Up
+// DP O(n)
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let mut h1 = 0;
+        let mut h2 = 0;
+
+        for num in nums {
+            let temp = h2.max(h1 + num);
+            h1 = h2;
+            h2 = temp;
+        }
+
+        h1.max(h2)
+    }
+}
+
+// Memoization Bottom Up O(n)
 use std::collections::HashMap;
 
 impl Solution {
