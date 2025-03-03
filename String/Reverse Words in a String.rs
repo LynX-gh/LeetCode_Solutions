9e281d5b27d603da340d02a482cf8f339101f20a
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-04 01:51:38 +0530

    Reverse Words in a String

--- String/Reverse Words in a String.rs
+++ String/Reverse Words in a String.rs
@@ -4,3 +4,10 @@ impl Solution {
         words.into_iter().rev().collect::<Vec<&str>>().join(" ")
     }
 }
+
+// One liner
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        s.split_ascii_whitespace().rev().collect::<Vec<&str>>().join(" ")
+    }
+}
