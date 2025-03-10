07cc605a67047fe23c27b9415f92f2fd12940e5c
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-11 01:05:21 +0530

    Odd Even Linked List

--- LinkedList/Odd Even Linked List.cpp
+++ LinkedList/Odd Even Linked List.cpp
@@ -11,6 +11,47 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr) {
+            return head;
+        }
+
+        ListNode* odd_s = head;
+        ListNode* even_s = head->next;
+        ListNode* curr_odd = odd_s;
+        ListNode* curr_even = even_s;
+        ListNode* curr = head->next->next;
+        bool odd = true;
+
+        while (curr != nullptr) {
+            if (odd) {
+                curr_odd->next = curr;
+                curr_odd = curr_odd->next;
+            } else {
+                curr_even->next = curr;
+                curr_even = curr_even->next;
+            }
+            curr = curr->next;
+            odd = !odd;
+        }
+        curr_odd->next = even_s;
+        curr_even->next = nullptr;
+        return odd_s;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
         if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
             return head;
         }
