5df136ab5e6f7f93c43eb6726ff7a01e9462ddd2
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-13 01:59:00 +0530

    Binary Tree Right Side View

--- Trees/Binary Tree Right Side View.cpp
+++ Trees/Binary Tree Right Side View.cpp
@@ -39,3 +39,45 @@ public:
         return res;
     }
 };
+
+// Using Depth
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        int d = 0;
+
+        queue<pair<TreeNode*, int>> q;
+        if (root != nullptr) {
+            q.push({root, 0});
+        }
+
+        while (!q.empty()) {
+            auto [ curr, depth ] = q.front();
+            q.pop();
+
+            if (depth == d) {
+                res.push_back(curr->val);
+                d++;
+            }
+            if (curr->right != nullptr) {
+                q.push({curr->right, depth+1});
+            }
+            if (curr->left != nullptr) {
+                q.push({curr->left, depth+1});
+            }
+        }
+        return res;
+    }
+};
