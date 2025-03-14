f3b94a786a4474dd30af10dec4f5b65dfb2c08f9
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-15 01:08:46 +0530

    Binary Search

---
+++ Binary Search/Longest Increasing Subsequence.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+        let mut sis = vec![];
+
+        for i in 0..nums.len() {
+            if let Err(idx) = sis.binary_search(&nums[i]) {
+                if idx == sis.len() {
+                    sis.push(nums[i]);
+                } else {
+                    sis[idx] = nums[i];
+                }
+            }
+        }
+
+        sis.len() as i32
+    }
+}
