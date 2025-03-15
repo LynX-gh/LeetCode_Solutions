4593ff923f6ab185fbd270946452fa9479e9ed3d
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-16 01:21:24 +0530

    Delete Node in a BST

---
+++ Trees/Delete Node in a BST.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* findMin(TreeNode* node) {
+        while(node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if (root->left == nullptr) {
+                return root->right;
+            } else if (root->right == nullptr) {
+                return root->left;
+            }
+
+            TreeNode* minNode = findMin(root->right);
+            root->val = minNode->val;
+            root->right = deleteNode(root->right, root->val);
+        }
+        return root;
+    }
+};
