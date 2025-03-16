4847298578609e1494a306f25266fcd1ad58807e
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-17 01:04:03 +0530

    Container With Most Water

--- Arrays/Container With Most Water.rs
+++ Arrays/Container With Most Water.rs
@@ -1,19 +1,17 @@
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let mut start_ptr = 0;
-        let mut end_ptr = height.len()-1;
-        let mut max_cap = 0;
+        let mut res = 0;
+        let mut i = 0;
+        let mut j = height.len()-1;
 
-        while start_ptr < end_ptr {
-            if height[start_ptr] < height[end_ptr] {
-                max_cap = i32::max(max_cap, (height[start_ptr] * (end_ptr - start_ptr) as i32));
-                start_ptr += 1;
-            }
-            else {
-                max_cap = i32::max(max_cap, (height[end_ptr] * (end_ptr - start_ptr) as i32));
-                end_ptr -= 1;
+        while i < j {
+            res = res.max((j - i) as i32 * height[i].min(height[j]));
+            if height[i] < height[j] {
+                i += 1;                
+            } else {
+                j -= 1;
             }
         }
-        max_cap
+        res
     }
 }
