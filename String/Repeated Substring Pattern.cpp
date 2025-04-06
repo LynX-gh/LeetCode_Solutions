6973d68211b987a0742a177b115c5ea9b8804aca
Khush Ramani <76166621+LynX-gh@users.noreply.github.com>
2025-04-07 01:33:26 +0530

    Repeated Substring Pattern

---
+++ String/Repeated Substring Pattern.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string doubled = s + s;
+        string sub = doubled.substr(1, doubled.size() - 2);
+        return sub.find(s) != string::npos;
+    }
+};
