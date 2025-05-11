a509091a9935ab0009d886eb355af13895a621d3
LynX-gh <76166621+LynX-gh@users.noreply.github.com>
2025-05-12 01:25:37 +0530

    User Activity for the Past 30 Days I

---
+++ SQL/User Activity for the Past 30 Days I.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    activity_date as day,
+    COUNT(DISTINCT(user_id)) as active_users
+FROM
+    Activity
+WHERE
+    activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY
+    activity_date
